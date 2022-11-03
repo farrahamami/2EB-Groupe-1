03065c8734679b3dd02abf1c13152de90e1badcc
farrahamami <115708082+farrahamami@users.noreply.github.com>
2022-11-03 21:49:00 +0100

    Add files via upload

---
+++ TP 3/code.js
@@ -0,0 +1,20 @@
+function A(){
+	alert("Chargement...");
+}
+
+function B(){
+	alert("Bye!");
+}
+
+function verif(s){
+	if ((s==0)||(s==1)||(s==11)) {
+		alert("Hiver");
+	}
+	else if ((s==2)||(s==3)||(s==4)) {
+		alert("Printemps");
+	}
+	else if ((s==5)||(s==6)||(s==7)) {
+		alert("Eté");
+	}
+	else alert("Automne")
+}
---
+++ TP4/code.js
@@ -0,0 +1,13 @@
+function A(){
+	f.p.disabled=false;
+}
+
+function B(){
+	f.c.disabled=false;
+}
+function D(){
+	if ((f.l.value=='admin')&&(f.p.value=='admin')) {
+		alert('success')
+	}
+	else alert('echec')
+}
