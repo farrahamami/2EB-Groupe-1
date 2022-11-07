16b5fa3051cdb978c8bd214ea0d5c3ce71a0c40f
Nour Yarmani <115720196+NourAyermani@users.noreply.github.com>
2022-11-07 19:46:42 +0100

    Update login.js

--- projet ds1/login.js
+++ projet ds1/login.js
@@ -1,6 +1,7 @@
+function togglemenu(){
+	document.getElementById('sidebar').classList.toggle('active');
+}
 const inputs = document.querySelectorAll(".input");
-
-
 function addcl(){
 	let parent = this.parentNode.parentNode;
 	parent.classList.add("focus");
@@ -12,17 +13,16 @@ function remcl(){
 		parent.classList.remove("focus");
 	}
 }
-
-
 inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 })
+function verif1(){
+	n = document.getElementById("t1").value;
+	if (/[دظ زوةى رؤءذط ك م ن ت ال ب ي س ش ج ح خ ه ع غ ف ق ث ص ض]/.test(n)==false)
+	 {
+		alert("الرجاء التثبت من الاسم")
+	 }
+ }
 
 
-
-
-
-function togglemenu(){
-	document.getElementById('sidebar').classList.toggle('active');
-}
